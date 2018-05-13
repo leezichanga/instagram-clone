dbac016a411fc6d271d67736074258e6cfc2cc41
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-13 13:00:37 +0300

    import models image,profile and comment in views

--- instagram/insta/views.py
+++ instagram/insta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse,Http404
+from .models import Image,Profile,Comment
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
