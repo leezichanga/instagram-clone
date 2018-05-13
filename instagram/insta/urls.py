8ac4751bc31588e3fe8415aa32bd872530c4b73e
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-13 13:30:29 +0300

    create edit url

--- instagram/insta/urls.py
+++ instagram/insta/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+
 
 urlpatterns=[
     url('^$',views.home,name='home'),
     url(r'^profile/',views.profile, name='profile'),
+    url(r'^settings/',views.settings,name='settings'),
+    url(r'^edit/',views.edit,name='edit'),
     ]
