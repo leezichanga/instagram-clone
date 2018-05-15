ccca196b34eca42e41851d43919c714316b54cf9
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-15 10:52:26 +0300

    update css

--- insta/tests.py
+++ insta/tests.py
@@ -10,10 +10,10 @@ class ProfileTestClass(TestCase):
         self.new_user = User(username="liz",email="elizabethichanga@yahoo.com")
         self.new_user.save()
         #set up profile class
-        self.liz=Profile(bio="I am awesome",user=self.new_user)
-        self.liz.save_profile()
+        self.Profile(bio="Feeling natural",user=self.new_user)
+        self.save_profile()
 
-        # self.user.add(self)
+        # self.user.add(self.liz)
 
     def tearDown(self):
         User.objects.all().delete()
@@ -21,26 +21,26 @@ class ProfileTestClass(TestCase):
 
     #testing instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.vicky,Profile))
+        self.assertTrue(isinstance(self.Profile))
 
     def test_save_profile(self):
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles)>0)
 
     def test_delete_profile(self):
-        self.liz.save_profile()
-        self.liz.delete_profile()
+        self.save_profile()
+        self.delete_profile()
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles)<1)
 
     def test_find_profile(self):
-        self.liz.save_profile()
+        self.save_profile()
         me = Profile.objects.all()
-        profiles = Profile.find_profile('vic')
+        profiles = Profile.find_profile('liz')
         self.assertEqual(profiles,profiles)
 
     def test_get_profile(self):
-        self.liz.save_profile()
+        self.save_profile()
         prof = Profile.get_profile()
         self.assertEqual(len(prof),1)
 
@@ -80,41 +80,3 @@ class ImageTestClass(TestCase):
 
     def test_get_image_by_id(self):
         pass
-
-
-class CommentTestClass(TestCase):
-    def setUp(self):
-        #set up user class
-        self.new_user = User(username="liz",email="elizabethichanga@yahoo.com")
-        self.new_user.save()
-        #set up for profile class
-        self.new_profile=Profile(bio="I am awesome",user=self.new_user)
-        self.new_profile.save()
-        #set up for Image class
-        self.flower=Image(caption="legacy goals",likes=200,profile=self.new_profile)
-        self.flower.save()
-        #set up for comment class
-        self.new_comment=Comment(comments="I love! All the best",user=self.new_user,image=self.car)
-        self.new_comment.save_comment()
-
-    def tearDown(self):
-        Image.objects.all().delete()
-        User.objects.all().delete()
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_comment,Comment))
-
-    def test_save_comment(self):
-        comments = Comment.objects.all()
-        self.assertTrue(len(comments)>0)
-
-    def test_delete_comment(self):
-        self.new_comment.save_comment()
-        self.new_comment.delete_comment()
-        comments = Comment.objects.all()
-        self.assertTrue(len(comments)<1)
-
-    def test_get_comment(self):
-        self.new_comment.save_comment()
-        comment = Comment.get_comment()
-        self.assertEqual(len(comment),1)
